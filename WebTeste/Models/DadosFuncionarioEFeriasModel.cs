431e91e4665dfb2742a4e4e6377bc584f6f03484
Git-Mota <evaldojr.contato@gmail.com>
2023-10-25 09:20:02 -0300

    att

--- WebTeste/Models/DadosFuncionarioEFeriasModel.cs
+++ WebTeste/Models/DadosFuncionarioEFeriasModel.cs
@@ -8,7 +8,7 @@ namespace WebTeste.Models
 
         public string Nome { get; set; }
         public string CPF { get; set; }
-        public string Departamento { get; set; }
+        public string? Departamento { get; set; }
         public string Localidade { get; set; }
         public DateTime? DataInicioFerias { get; set; }
         public DateTime? DataTerminoFerias { get; set; }
