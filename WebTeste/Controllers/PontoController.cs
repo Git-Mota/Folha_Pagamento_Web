8dd79ec9fcfcd3ab8e2f826962eed1017b41c652
Git-Mota <evaldojr.contato@gmail.com>
2023-10-29 20:07:19 -0300

    att

--- WebTeste/Controllers/PontoController.cs
+++ WebTeste/Controllers/PontoController.cs
@@ -29,7 +29,17 @@ namespace WebTeste.Controllers
         }
         public IActionResult CriarPonto()
         {
-            return View();
+            var identity = User.Identity as ClaimsIdentity;
+            var idFunClaim = identity.FindFirst("Id_Func");
+            int convert = int.Parse(idFunClaim.Value);
+            var pontoSQL = _bancoContext.Tab_Ponto.FromSqlRaw("SELECT Tab_Ponto.db_IdPonto AS 'IdPonto', Tab_Ponto.db_IdFuncionario AS 'IdFuncionario',Tab_Funcionario.NomeCompleto AS Nome," +
+                 "Tab_Funcionario.tb_Departamento AS Departamento, Tab_Ponto.db_Data AS Data," +
+                 "Tab_Ponto.db_DiaSemana AS 'DiaSemana', Tab_Ponto.db_HoraSaida AS 'HoraSaida'," +
+                 "Tab_Ponto.db_HoraEntrada AS 'HoraEntrada', Tab_Ponto.db_HoraInicioAlmoco AS 'HoraInicioAlmoco'," +
+                 "Tab_Ponto.db_db_HoraRetornoAlmoco AS 'HoraRetornoAlmoco', Tab_Ponto.db_HoraInicioPausa AS 'HoraInicioPausa'," +
+                 "Tab_Ponto.db_HoraRetornoPausa AS 'HoraRetornoPausa'" +
+                 "FROM Tab_Funcionario LEFT JOIN Tab_Ponto ON Tab_Funcionario.Id = Tab_Ponto.db_IdFuncionario WHERE Tab_Funcionario.Id =" + convert).ToList();
+            return View(pontoSQL);
         }
     }
 }
