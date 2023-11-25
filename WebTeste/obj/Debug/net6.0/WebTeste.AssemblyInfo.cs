a764a89a94ad73b0220270d08ab35f5dbbc1d8c4
Git-Mota <evaldojr.contato@gmail.com>
2023-11-25 15:27:28 -0300

    att

--- WebTeste/obj/Debug/net6.0/WebTeste.AssemblyInfo.cs
+++ WebTeste/obj/Debug/net6.0/WebTeste.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTeste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92a36b052cc2d6b0dcb759aa0b978c5db2091df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93eff3b5acd701976ca658276d0a6f1f360f4877")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTeste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTeste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
